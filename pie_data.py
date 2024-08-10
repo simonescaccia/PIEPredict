8c66bb3f67958f808cd8bb3a05a28e3eb6ae7439
simonescaccia <72872543+simonescaccia@users.noreply.github.com>
2024-08-10 04:06:35 +0200

    Fix default dataset

--- pie_data.py
+++ pie_data.py
@@ -102,15 +102,11 @@ class PIE(object):
         :param image_set: Image set split
         :return: Set ids of the image set
         """
-        image_set_nums = {'train': ['set02'],
-                          'val': ['set05'],
-                          'test': ['set05'],
-                          'all': ['set02','set05']}
-        # image_set_nums = {'train': ['set01', 'set02', 'set04'], TODO: restore original split
-        #                   'val': ['set05', 'set06'],
-        #                   'test': ['set03'],
-        #                   'all': ['set01', 'set02', 'set03',
-        #                           'set04', 'set05', 'set06']}
+        image_set_nums = {'train': ['set01', 'set02', 'set04'],
+                          'val': ['set05', 'set06'],
+                          'test': ['set03'],
+                          'all': ['set01', 'set02', 'set03',
+                                  'set04', 'set05', 'set06']}
         return image_set_nums[image_set]
 
     def _get_image_path(self, sid, vid, fid):
